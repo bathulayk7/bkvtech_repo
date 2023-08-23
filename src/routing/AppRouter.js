fac5b7f56c2740a8ac2f3c71c0b9339f540f9490
bathulayedukondalu <bathulayedukondalu@gmail.com>
2023-08-23 19:43:29 +0530

    commmit1

--- src/routing/AppRouter.js
+++ src/routing/AppRouter.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import ErrorPage from '../pages/ErrorPage'
 import About from '../pages/About'
+import Contact from '../pages/Contact'
 
 const AppRouter = () => {
   return (
@@ -10,6 +11,7 @@ const AppRouter = () => {
         <Route index  element={<Home/>} />
         <Route path="/home" element={<Home/>} />
         <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/> } />
         <Route path="/*" element={<ErrorPage/>} />
     </Routes>
   )
--- src/routing/Header.js
+++ src/routing/Header.js
@@ -1,13 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../images/logo.png";
 
 const Header = () => {
   return (
-    <div>
-        <Link to="home">Home</Link>
-        <Link to="about">About</Link>
-    </div>
-  )
-}
+    <header className="header_container">
+      <nav className="navbar">
+        <Link to="home" className="logo_siteName">
+          <img src={logo} alt="logo" />
+          <h2>bkvtech</h2>
+        </Link>
+        <nav className="navLinks">
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "activeLink" : "inActiveLink"
+            }
+            to="home"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "activeLink" : "inActiveLink"
+            }
+            to="about"
+          >
+            About
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "activeLink" : "inActiveLink"
+            }
+            to="contact"
+          >
+            Contact
+          </NavLink>
+        </nav>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
